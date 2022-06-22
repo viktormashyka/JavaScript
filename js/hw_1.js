284160c994533714328922879c858fd6c2e494aa
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2022-06-22 22:01:26 +0100

    Починає писати код JS!

---
+++ js/hw_1.js
@@ -0,0 +1,20 @@
+// alert('Hello World!');
+// prompt('Hello World!!');
+
+console.log('Це твій перший код!');
+console.log('Вітаю!');
+
+let a = prompt('Задай "A"');
+let b = prompt('Задай "B"');
+
+let message;
+
+// if (Number(a) < Number(b)) {
+//   message = 'Haw are you?';
+// } else {
+//   message = 'Fuck you!';
+// }
+
+message = Number(a) < Number(b) ? 'Haw are you?' : 'Fuck you!';
+
+message = console.log(message);
