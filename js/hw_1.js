f3f61631b72fbfb1147b2aaf99614c36a838a421
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2022-06-26 23:22:44 +0100

    Update hw_1.js

--- js/hw_1.js
+++ js/hw_1.js
@@ -1,20 +1,39 @@
-// alert('Hello World!');
-// prompt('Hello World!!');
+// // alert('Hello World!');
+// // prompt('Hello World!!');
 
-console.log('Це твій перший код!');
-console.log('Вітаю!');
+// console.log('Це твій перший код!');
+// console.log('Вітаю!');
 
-let a = prompt('Задай "A"');
-let b = prompt('Задай "B"');
+// let a = prompt('Задай "A"');
+// let b = prompt('Задай "B"');
 
-let message;
+// let message;
 
-// if (Number(a) < Number(b)) {
-//   message = 'Haw are you?';
-// } else {
-//   message = 'Fuck you!';
-// }
+// // if (Number(a) < Number(b)) {
+// //   message = 'Haw are you?';
+// // } else {
+// //   message = 'Good bye!';
+// // }
 
-message = Number(a) < Number(b) ? 'Haw are you?' : 'Fuck you!';
+// message = Number(a) < Number(b) ? 'Haw are you?' : 'Good bye!';
 
-message = console.log(message);
+// message = console.log(message);
+
+// /**** Форматування посилання (includes та логічне «І») ****/
+
+// /*
+// Умова завдання:
+// Напиши скрипт який перевіряє чи закінчується значення змінної link символом /.
+// Якщо ні, додай у кінець значення link цей символ, але тільки в тому випадку, якщо в link є підрядок "my-site".
+// */
+
+// let link = 'https://my-site.com/about';
+
+const target = 3;
+let sum = 0;
+
+for (let i = 0; i <= target; i += 1) {
+  sum += i;
+}
+
+console.log(sum);
